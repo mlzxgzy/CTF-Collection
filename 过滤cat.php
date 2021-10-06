ed5c0fffeebe41f56eeb2aa4a1f6abad4fa4b925
LPLaser <mlzxgzy@gmail.com>
2021-10-06 12:24:19 +0800

    Create 过滤cat.php

---
+++ 过滤cat.php
@@ -0,0 +1,21 @@
+<?php
+header("Content-type: text/html; charset=utf-8");
+show_source(__FILE__);
+$res = FALSE;
+echo '<!--index.php?ip=-->';
+
+if (isset($_GET['ip']) && $_GET['ip']) {
+    $ip = $_GET['ip'];
+    $m = [];
+    if (!preg_match_all("/cat/", $ip, $m)) {
+        $cmd = "ping -c 4 {$ip}";
+        exec($cmd, $res);
+    } else {
+        $res = $m;
+    }
+}
+if ($res) {
+    print_r($res);
+}
+
+?>
